695ee5d2e13bcea5baf973293af05a6f601d28ab
dbk14 <dbk14@student.le.ac.uk>
2022-12-11 21:44:58 +0000

    testing...

--- server/redis.mjs
+++ server/redis.mjs
@@ -8,8 +8,9 @@ const {
 } = process.env
 
 export const connection = createClient({
-  url: "rediss://icc1.redis.cache.windows.net:6380",
-  password: "0SItI0tLy1ODHHZqHUhjIwpe3DGeSFYiyAzCaJVtEYs=",
+  url: "rediss://iccredis.redis.cache.windows.net:6380",
+  //password: "0SItI0tLy1ODHHZqHUhjIwpe3DGeSFYiyAzCaJVtEYs=",
+  password: "ePFDKgpvsrJlrdozTbKQAj9dehbTPCtpBAzCaGegfWc=",
 
 });
 await connection.connect()
